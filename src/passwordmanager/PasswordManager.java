fd10db15948c81754f246dee6b9736fdc33bac04
Matthew Mateusz <mateuszm@matthewm.com.pl>
2018-10-18 07:45:36 -0500

    New Guis
    Created new guis for the program.
    Added new logic at the beggining of the program to check if password file exsist and to prompt with "do you want to create one" if it does not exsists.

--- src/passwordmanager/PasswordManager.java
+++ src/passwordmanager/PasswordManager.java
@@ -5,6 +5,9 @@
  */
 package passwordmanager;
 
+import java.io.File;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Matthew
@@ -15,13 +18,33 @@ public class PasswordManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        boolean runProgram = false;
         MainGui passwordGui = new MainGui();
         
         //Todo: Check for encrypted password file.
         // If there is decrypt and read.
         // If there is not promt if they want to create a new one.
-        
         passwordGui.setVisible(true);
+        File passwordFile = new File("data.epf");
+        if (passwordFile.exists() && !passwordFile.isDirectory()) 
+        {
+            runProgram = true;
+        }
+        else {
+            Object[] options = {"Yes", "No"};
+            int r = JOptionPane.showOptionDialog(
+                    passwordGui, 
+                    "Could not find password file \n Do you want to create one", 
+                    "Do you want to create a password file", 
+                    JOptionPane.YES_NO_OPTION, 
+                    JOptionPane.WARNING_MESSAGE, 
+                    null, 
+                    options, 
+                    options[1]);
+            
+        }
+        
+            
     }
     
 }
